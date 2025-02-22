1d31da1fab353cc6f6e0eb56c5c6464566e0e571
AlmironJoel <brn_joel@hotmail.com>
2025-02-21 21:02:00 -0300

    Ejer.cpp

---
+++ ejer.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Dada una lista de 10 números enteros, cargarlos en un vector. Luego,
+// determinar e informar si el vector está ordenado en forma creciente. Por
+// ejemplo el vector con los valores 1, 3, 5, 7 y 9 está ordenado; el vector 1, 5, 3, 7
+// y 9 no lo está.
+
+//Funciones
+int cargarNumeros (int n[10]){
+    for (int i = 0; i < 10; i++)
+    {
+    int num;
+    cout<<"Introduzca numero...";
+    cin>> num;
+    n[i]=num;
+    }
+}
+
+// int verificarOrden(Vnum[10]){
+
+// }
+
+int mostrarNumeros(int n[10]){
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<n[i]<<endl;
+    }
+}
+
+//Main
+int main(int argc, char const *argv[])
+{
+    int Vnum[10];
+    cargarNumeros(Vnum);
+    // verificarOrden(Vnum);
+    mostrarNumeros(Vnum);
+
+    return 0;
+}
