aa0c89ed9ecbe0663c2308032c67a68c057538a6
AlmironJoel <brn_joel@hotmail.com>
2025-03-06 22:16:49 -0300

    metodo burbuja completado

--- metodoBurbuja.cpp
+++ metodoBurbuja.cpp
@@ -8,25 +8,45 @@ using namespace std;
 
 //?Funciones
 
-void ingresarNum(int Vnum[10]){
+void ingresarNum(int Vnum[10],int orden[10]){
     int num;
     for (int i = 0; i < 10; i++){
         cout<<"ingrese numero"<<endl;
         cin>>num;
         Vnum[i]=num;
+        orden[i]=i+1;
     }
-    cout<<"Lista de numeros Cargados!";
+    cout<<"Lista de numeros Cargados!"<<endl;
 }
 
-void copiarArray(){
-    
+void mostrarArray(int Vnum[10],int orden[10]){
+    for (int i = 0; i < 10; i++){
+        cout<<"El Numero: "<<Vnum[i]<<" Se encontraba originalmente en el indice : "<<orden[i]<<endl;
+    }
+}
+
+void proceso(int Vnum[10],int orden[10]){
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 9; j++){
+            if (Vnum[j]>Vnum[j+1]){
+                int aux;
+                aux=Vnum[j+1];
+                Vnum[j+1]=Vnum[j];
+                Vnum[j]=aux;
+                aux=orden[j+1];
+                orden[j+1]=orden[j];
+                orden[j]=aux;
+            }
+          }
+         }
+
+    mostrarArray(Vnum,orden);
 }
 
 int main(int argc, char const *argv[]){
     int Vnum[10];
-    ingresarNum(Vnum);
-    int Vcopia[10];
-    copiarArray(Vcopia);
-
+    int orden[10];
+    ingresarNum(Vnum,orden);
+    proceso(Vnum,orden);
     return 0;
 }
